ba678ce57183a29645f5492ddab3c9122345093e
yyyan97 <35853687+yyyan97@users.noreply.github.com>
2019-08-19 11:04:26 -0400

    changing db location

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-lively-94685',
     user : '',
     password : '',
     database : 'smart-brain'
