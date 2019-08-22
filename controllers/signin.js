a814677e2abd33a9bf2f7f80c4bab24a4a4d4ffa
yyyan97 <35853687+yyyan97@users.noreply.github.com>
2019-08-22 14:09:59 -0400

    change response

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,7 +1,7 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('Please fill out the form');
   }
   db.select('email', 'hash').from('login')
     .where('email', '=', email)
@@ -13,12 +13,12 @@ const handleSignin = (db, bcrypt) => (req, res) => {
           .then(user => {
             res.json(user[0])
           })
-          .catch(err => res.status(400).json('unable to get user'))
+          .catch(err => res.status(400).json('Unable to get user'))
       } else {
-        res.status(400).json('wrong credentials')
+        res.status(400).json('Wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => res.status(400).json('Wrong credentials'))
 }
 
 module.exports = {
