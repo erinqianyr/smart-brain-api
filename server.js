2690ec8c236ab74582a4c48748b2dc0e0dbf3c1f
yyyan97 <35853687+yyyan97@users.noreply.github.com>
2019-08-23 12:46:46 -0400

    cors

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', (req, res)=> { res.send("IT'S WORKING") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
